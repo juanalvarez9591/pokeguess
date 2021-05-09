7d46a320765f77334fdd0f2df3980c79ef684733
Juan <juanalvarez9591@gmail.com>
2021-05-09 03:21:29 -0300

    ;)

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-let API = "https://pokeapi.co/api/v2/pokemon/";
-let PokeList = [];
+// API url
+const API = "https://pokeapi.co/api/v2/pokemon/";
 
-for (let i=1; i != 152; i++) {
-    PokeList.push(i.toString());
+// We execute the function new game for the first time
+let PokeList;
+
+function NewGame(){
+    PokeList = [];
+
+    for (let i=1; i != 152; i++) {
+        PokeList.push(i.toString());
+    }
 }
 
+NewGame();
+
+// This function removes a Pokemon from the PokeList (it isn't directly called)
 function GrabPokemon(){
     let PokeIndex = Math.floor(Math.random() * PokeList.length);
     PokeList.splice(PokeIndex, 1);
     return(PokeList[PokeIndex]);
 }
 
+// This configures an APICall to get the name and sprite of a Pokemon
 const APICall = async () => {
     const response = await fetch(API+GrabPokemon());
     const JSONData = await response.json();
@@ -21,20 +32,30 @@ const APICall = async () => {
     return(Pokemon);
 }
 
+// This function spawns in the div the Pokemon by making an API Call
 const Spawn = async () => {
+    document.getElementById("nextbutton").textContent = "Next"
+
     const data = await APICall();
 
-    const container = document.getElementById("container");
-    const paragraph = document.createElement("p");
-    const img = document.createElement("img");
+    const paragraph = document.getElementById("poke0");
+    const img = document.getElementById("img0");
 
     paragraph.textContent = data.name;
     img.src = data.sprite
-
-    container.appendChild(paragraph);
-    container.appendChild(img);
-
-    
 }
 
-Spawn()
+
+// When the DOM is loaded we start to do stuff
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("nextbutton").textContent = "New Game"
+    document.getElementById("nextbutton").onclick = Spawn;
+});
+
+
+
+
+
+
+
+
